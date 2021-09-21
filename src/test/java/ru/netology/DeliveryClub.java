62c210311b71c1db01e3df4069651d7d664dfe33
Aleksey Efimov <Aleks4404@mail.ru>
2021-09-21 16:31:38 +0400

    Create auto-test

--- src/test/java/ru/netology/DeliveryClub.java
+++ src/test/java/ru/netology/DeliveryClub.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.concurrent.TimeUnit;
 
@@ -12,6 +14,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeliveryClub {
     private WebDriver driver;
+    private WebDriverWait wait;
 
     @BeforeAll
     static void setUpAll() {
@@ -21,6 +24,7 @@ public class DeliveryClub {
     @BeforeEach
     void setUp() {
         driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 5);
     }
 
     @AfterEach
@@ -30,7 +34,7 @@ public class DeliveryClub {
 
     @Test
     @DisplayName("Поиск Картошки фри")
-    void shouldTestV1() {
+    void shouldFindTheProduct() {
         driver.get("https://www.delivery-club.ru/samara");
         driver.findElement(By.className("header__search-button__name")).click();
         driver.findElement(By.className("vendors-suggests__input")).sendKeys("Картошка фри" + Keys.ENTER);
@@ -38,5 +42,31 @@ public class DeliveryClub {
         String expected = "Картошка фри";
         String actual = driver.findElement(By.className("search-results__query")).getText();
         assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Авторизация с невалидным номером")
+    void shouldLogInWithAnInvalidNumber() {
+        driver.get("https://www.delivery-club.ru/samara");
+        driver.findElement(By.className("header-login-button")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//div[2]/div[1]/div[1]/label/input")).sendKeys("0000000000" + Keys.ENTER);
+        String expected = "Не удалось отправить код";
+        String actual = driver.findElement(By.className("label--def__error")).getText();
+        assertEquals(expected, actual);
+    }
+    @Test
+    @DisplayName("Авторизация с валидным номером")
+    void shouldLogInWithValidNumber() {
+        driver.get("https://www.delivery-club.ru/samara");
+        driver.findElement(By.className("header-login-button")).click();
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+        driver.findElement(By.xpath("//div[2]/div[1]/div[1]/label/input")).sendKeys("9379855181" + Keys.ENTER);
+        //Wait until alert is present
+        wait.until(ExpectedConditions.alertIsPresent());
+        driver.switchTo().alert().accept();
+        String ExpectedConditions = "Код отправлен на номер";
+        String Actual = driver.findElement(By.xpath("//body/div[3]//div[2]//div[2]//div[2]/div[1]")).getText();
+        assertEquals(ExpectedConditions, Actual);
+//        /html/body/div[3]/div/div[2]/div/div/div[2]/div/div[2]/div[1]
     }
 }
