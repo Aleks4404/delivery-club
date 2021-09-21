e6f3840c3c36e9debdb297c1ebfa3d603da79ee9
Aleksey Efimov <Aleks4404@mail.ru>
2021-09-21 13:27:37 +0400

    Create auto-test

--- src/test/java/ru/netology/DeliveryClub.java
+++ src/test/java/ru/netology/DeliveryClub.java
@@ -1,8 +1,14 @@
 package ru.netology;
 
 import org.junit.jupiter.api.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DeliveryClub {
     private WebDriver driver;
@@ -20,12 +26,17 @@ public class DeliveryClub {
     @AfterEach
     void tearDown() {
         driver.quit();
-        driver = null;
     }
 
     @Test
-    @DisplayName("Вход в систему пользователя с помощью логина")
+    @DisplayName("Поиск Картошки фри")
     void shouldTestV1() {
         driver.get("https://www.delivery-club.ru/samara");
+        driver.findElement(By.className("header__search-button__name")).click();
+        driver.findElement(By.className("vendors-suggests__input")).sendKeys("Картошка фри" + Keys.ENTER);
+        driver.manage().timeouts().pageLoadTimeout(10000, TimeUnit.MILLISECONDS);
+        String expected = "Картошка фри";
+        String actual = driver.findElement(By.className("search-results__query")).getText();
+        assertEquals(expected, actual);
     }
 }
